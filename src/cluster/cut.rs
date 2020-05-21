11aa6ff4f7aa1c10b47399a45adfd6e371bab913
nytopop <ericizoita@gmail.com>
2020-05-20 20:05:27 -0700

    cluster: use as_deref() (clippy hint)

--- src/cluster/cut.rs
+++ src/cluster/cut.rs
@@ -175,7 +175,7 @@ impl Member {
     /// Returns a reference to the tls configuration that will be used for outgoing conns
     /// to this member, or `None` if it isn't expecting tls.
     pub fn tls_config(&self) -> Option<&ClientTlsConfig> {
-        self.tls.as_ref().map(|tls| &**tls)
+        self.tls.as_deref()
     }
 
     /// Returns a reference to the member's metadata.
